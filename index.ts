f4a170a2673cb0727d29c7b8f246a64f54d7eccd
Neeraj-x0 <krishnaneeraj773@gmail.com>
2024-11-20 11:37:49 +0530

    Add dotenv configuration and start script to package.json

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import weatherRouter from "./src/routes/weather";
 import redisClient from "./src/configs/redis";
-import dotenv from "dotenv";
 import locationRouter from "./src/routes/location";
 import cors from "cors";
-dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
